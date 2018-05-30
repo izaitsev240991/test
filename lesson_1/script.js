758bb213789633c20ea7d7da56287dc26584c22f
Ilia <izaitsev240991@gmail.com>
2018-05-30 23:00:58 +0300

    исправленная работа

--- lesson_1/script.js
+++ lesson_1/script.js
@@ -1,23 +1,22 @@
 var budget = prompt("Ваш бюджет на месяц?", "");
 var shopName = prompt("Название вашего магазина", ""); 
+var mainList = {
+	'budget' : budget,
+	'shopName': shopName,
+	'shopGoods': shopGoods,
+	'employers': employers,
+	'open': open
+}
 var product1 = prompt("Какой тип товаров будем продавать?", "");
 var product2 = prompt("Какой тип товаров будем продавать?", "");
 var product3 = prompt("Какой тип товаров будем продавать?", "");
-var open = window.open("about:blank");
-
 var shopGoods = [product1, product2, product3]
-
-employers = {
+var employers = {
 	name: "Вася",
 	age: "22",
 	position: "кладовщик"
 }
+var open = window.open("about:blank");
 
-mainList = {
-	budget,
-	shopName,
-	shopGoods,
-	employers,
-	open
-}
+
 alert("Ваш бюджет на 1 день: " + budget/30);
