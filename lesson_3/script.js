9a99fc8b443c2916735eb96b95e158c90a41736a
Ilia <izaitsev240991@gmail.com>
2018-06-01 16:23:27 +0300

    Урок 3

---
+++ lesson_3/script.js
@@ -0,0 +1,88 @@
+let money,
+ 		name,  
+ 		time = 19,
+ 		price = 100
+
+function start(){
+	 money = prompt("Ваш бюджет на месяц?");
+
+	 while(isNaN(money) || money == "" || money == null){
+	 	money = prompt("Ваш бюджет на месяц?");
+	 }
+
+	 name = prompt("Название вашего магазина").toUpperCase(); 
+	 
+	 
+}
+start();
+
+let mainList = {
+	budget : money,
+	shopName: name,
+	shopGoods: [],
+	employers: {},
+	open: false,
+	discount: true
+}
+
+function chooseGoods(){
+		for (let i = 0; i < 5; i++) {
+			let a = prompt("Какой тип товаров будем продавать?");
+			if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50 ){
+				console.log('Всё верно');
+				mainList.shopGoods[i] = a;
+			} else  {
+				alert('введите товары');
+				i--;
+			};
+		}
+}
+chooseGoods();
+
+function workTime(time){
+		if(time < 0){
+			console.log('Такого не может быть');
+		} else if(time > 8 && time < 20){
+				console.log('Рабочее время');
+				} else if(time < 24) {
+					console.log('Уже слишком поздно');
+						} else{
+							console.log('В сутках 24 часа');
+							};
+}
+workTime(time);
+
+function dayBudget(){
+	alert("Ваш бюджет на 1 день: " + mainList.budget/30);
+
+}
+dayBudget();
+
+function getDiscount(price){
+	
+	if (mainList.discount == true) {
+		 price *= .8;
+		 console.log(price);
+		} else{
+			console.log(price);
+			};
+}
+getDiscount(price);
+
+
+function takeEmployers(){
+	for (let i = 1; i < 5  ; i++) {
+		let w = prompt("Введите имя сотрудника");
+		if ((typeof(w)) === 'string' && (typeof(w)) != null && w != '' && w.length < 50) {
+			mainList.employers[i] = w;
+		} else{
+			alert('корректно введите имя');
+				i--;
+			};
+	}
+}
+takeEmployers();
+console.log(mainList);
+
+
+
